7bb2e36222102d905e8ff82f6e7bdbde07b03542
Hv0stjuk <wot.hvostjuk@gmail.com>
2021-07-06 18:45:13 +0300

    fix: replacing the tag a with navlink

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,9 @@ import vkIcon from '../../assets/images/icons/vk-icon.svg';
 import instaIcon from '../../assets/images/icons/instagram-icon.svg';
 import youtubeIcon from '../../assets/images/icons/youtube-icon.svg';
 import facebookIcon from '../../assets/images/icons/facebook-icon.svg';
+import { NavLink } from 'react-router-dom';
+
+
 
 export const Footer = () => {
     return (
@@ -11,7 +14,7 @@ export const Footer = () => {
             <div className={s.footerTop}>
                 <form className={s.footerForm} action="">
                     <h4 className={s.footerFormTitle}>
-                    Подпишитесь на нашу рассылку и узнавайте о акциях быстрее
+                    Подпишитесь на нашу расслку и узнайте о акциях быстрее
                     </h4>
                     <input className={s.footerInput} type="email" required placeholder="Введите Ваш e-mail"/>
                     <button className={s.footerBtn}>Отправить</button>
@@ -20,38 +23,38 @@ export const Footer = () => {
                     <h4 className={s.footerTitle}>
                         Информация
                     </h4>
-                    <a className={s.footerLink} href="#">О компании</a>
-                    <a className={s.footerLink} href="#">Контакты</a>
-                    <a className={s.footerLink} href="#">Акции</a>
-                    <a className={s.footerLink} href="#">Магазины</a>
+                    <NavLink to={'/'} className={s.footerLink}>О компании</NavLink>
+                    <NavLink to={'/'} className={s.footerLink}>Контакты</NavLink>
+                    <NavLink to={'/'} className={s.footerLink}>Акции</NavLink>
+                    <NavLink to={'/'} className={s.footerLink}>Магазины</NavLink>
                 </div>
                 <div className={s.footerShop}>
                     <h4 className={s.footerTitle}>
                     Интернет-магазин
                     </h4>
-                    <a className={s.footerLink} href="#">Доставка и самовывоз</a>
-                    <a className={s.footerLink} href="#">Оплата</a>
-                    <a className={s.footerLink} href="#">Возврат-обмен</a>
-                    <a className={s.footerLink} href="#">Новости</a>
+                    <NavLink to={'/'} className={s.footerLink}>Доставка и самовывоз</NavLink>
+                    <NavLink to={'/'} className={s.footerLink}>Оплата</NavLink>
+                    <NavLink to={'/'} className={s.footerLink}>Возврат-обмен</NavLink>
+                    <NavLink to={'/'} className={s.footerLink}>Новости</NavLink>
                 </div>
                 <div className={s.footerSocial}>
-                    <a className={s.footerSocialLink} href="#">
+                    <NavLink to={'/'} className={s.footerSocialLink}>
                         <img src={vkIcon} alt="img" />
-                    </a>
-                    <a className={s.footerSocialLink} href="#">
+                    </NavLink>
+                    <NavLink to={'/'} className={s.footerSocialLink}>
                         <img src={instaIcon} alt="img" />
-                    </a>
-                    <a className={s.footerSocialLink} href="#">
+                    </NavLink>
+                    <NavLink to={'/'} className={s.footerSocialLink}>
                         <img src={youtubeIcon} alt="img" />
-                    </a>
-                    <a className={s.footerSocialLink} href="#">
+                    </NavLink>
+                    <NavLink to={'/'} className={s.footerSocialLink}>
                         <img src={facebookIcon} alt="img" />
-                    </a>
+                    </NavLink>
                 </div>
             </div>
             <div className={s.footerBottom}>
-                <a className={s.footerBottomLink} href="#">Договор оферты</a>
-                <a className={s.footerBottomLink} href="#">Политика обработки персональных данных</a>
+                <NavLink to={'/'} className={s.footerBottomLink}>Договор оферты</NavLink>
+                <NavLink to={'/'} className={s.footerBottomLink}>Политика обработки персональных данных</NavLink>
             </div>
             </div>
         </footer>
