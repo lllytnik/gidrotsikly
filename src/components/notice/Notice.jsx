09890441be701d2a1a6ac5d3030ef002ed0cff3d
lllytnik <65924940+lllytnik@users.noreply.github.com>
2021-07-08 00:02:03 +0100

    add block notice

---
+++ src/components/notice/Notice.jsx
@@ -0,0 +1,26 @@
+import s from './Notice.module.scss';
+import pic from '../../assets/images/notice/pic-motor.png'
+function Notice() {
+    return (
+        <div className={s.notice}>
+            <div className={s.wrapper}>
+                <div className={s.boxLeft}>
+                    <h2 className={s.title}>Акция</h2>
+                </div>
+                <div className={s.boxRight}>
+                    <span className={s.priceNew}>190 000</span>
+                    <span className={s.priceOld}>225 000</span>
+                </div>
+            </div>
+            <img src={pic} alt="pic" />
+            <h3 className={s.subtitle}>Лодочный мотор
+                Suzuki DF9.9BRS</h3>
+            <div className={s.boxBotton}>
+                <span className={s.text}>Акция действует до</span>
+                <span className={s.data}>31.08.2020</span>
+            </div>
+        </div>
+    );
+}
+
+export default Notice;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,13 +2,18 @@ import s from './Main.module.scss';
 import NavigationBotton from '../../components/navigation/Navigation';
 import Header from '../../components/header/Header';
 import SliderTop from '../../components/slider/Slider';
+import Notice from '../../components/notice/Notice';
 export const Main = () => {
   return (
     <div className={s.container}>
       <main className={s.main}>
         <Header />
         <NavigationBotton />
-        <SliderTop />
+        <div className={s.inner}>
+          <SliderTop />
+          <Notice />
+        </div>
+
       </main>
     </div>
 
