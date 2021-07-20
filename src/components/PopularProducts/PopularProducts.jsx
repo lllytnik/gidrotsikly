4f4929ec80ace81758409c5a86bf21e7a5364cc6
Alexander Khvostyuk <wot.hvostjuk@gmail.com>
2021-07-20 20:21:27 +0300

    review: destructuring styles & props ProductTabs

--- src/components/PopularProducts/PopularProducts.jsx
+++ src/components/PopularProducts/PopularProducts.jsx
@@ -1,20 +1,34 @@
-import {popularProducts, popularProductsTitle, popularProductsContainer, popularProductsCardWrap} from '../../components/PopularProducts/PopularProducts.module.scss'
+import {
+  popularProducts,
+  popularProductsTitle,
+  popularProductsContainer,
+  popularProductsCardWrap,
+} from '../../components/PopularProducts/PopularProducts.module.scss';
 import { PopularProductCard } from '../PopularProductCard/PopularProductCard';
 import { ProductTabs } from '../../components/ProductTabs/ProductTabs';
 
-export const PopularProducts= () => {
-    return (
-        <section className={popularProducts}>
-            <div className={popularProductsContainer}>
-                <h2 className={popularProductsTitle}>Популярные товары</h2>
-                < ProductTabs />
-                <div className={popularProductsCardWrap}>
-                < PopularProductCard name="Водонепроницаемый рюкзак" price="9800"/>
-                < PopularProductCard />
-                < PopularProductCard />
-                < PopularProductCard />
-                </div>
-            </div>
-        </section>
-    );
+export const PopularProducts = () => {
+  return (
+    <section className={popularProducts}>
+      <div className={popularProductsContainer}>
+        <h2 className={popularProductsTitle}>Популярные товары</h2>
+        <ProductTabs
+          items={[
+            { name: 'запчасти' },
+            { name: 'моторы' },
+            { name: 'шины' },
+            { name: 'электроника' },
+            { name: 'инструменты' },
+            { name: 'аксессуары' },
+          ]}
+        />
+        <div className={popularProductsCardWrap}>
+          <PopularProductCard name="Водонепроницаемый рюкзак" price="9800" />
+          <PopularProductCard />
+          <PopularProductCard />
+          <PopularProductCard />
+        </div>
+      </div>
+    </section>
+  );
 };
