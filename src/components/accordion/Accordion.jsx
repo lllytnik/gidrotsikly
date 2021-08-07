61fdcd08350166517d91de91e05f7ec36ada6331
lllytnik <65924940+lllytnik@users.noreply.github.com>
2021-08-07 14:37:55 +0400

    working with the product page

---
+++ src/components/accordion/Accordion.jsx
@@ -0,0 +1,138 @@
+import s from './Accordion.module.scss';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CheckboxLabels from '../checkbox/Checkbox';
+import RangeSlider from '../range/Range';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
+export default function SimpleAccordion() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography className={classes.heading}>Наличие</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <div className={s.typographyWrap}>
+              <CheckboxLabels title={'В наличие'} />
+              <CheckboxLabels title={'Под заказ'} />
+            </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Новинки</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography className={s.typography}>
+            <div className={s.typographyWrap}>
+              <CheckboxLabels title={'Все'} />
+              <CheckboxLabels title={'Новинки'} />
+              <CheckboxLabels title={'Акции'} />
+            </div>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>Цена</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <RangeSlider />
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>Бренд</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>Модель</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>Акции</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography className={classes.heading}>Страны</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
--- src/pages/JetSkis/JetSkis.jsx
+++ src/pages/JetSkis/JetSkis.jsx
@@ -4,6 +4,7 @@ import { Header } from '../../components/header/Header';
 import { Navigation } from '../../components/navigation/Navigation';
 import { CustomSeparator } from '../../components/breadcrumbs/BreadCrumb';
 import { SortingProduct } from '../../components/product-top-block/SortingProduct';
+import TabPanel from '../../components/paper/paper';
 
 export const JetSkis = () => {
   return (
@@ -15,6 +16,9 @@ export const JetSkis = () => {
           <CustomSeparator />
         </div>
         <SortingProduct />
+      </div>
+      <div className={s.parameters}>
+        <TabPanel />
       </div>
     </div>
   );
